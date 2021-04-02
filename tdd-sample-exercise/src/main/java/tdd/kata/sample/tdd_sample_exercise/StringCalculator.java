401778b79a3a029a64951566035d53ade61d248d
shivamkarodia <shivam.karodia@gmail.com>
2021-04-02 19:48:53 +0530

    Step 5:- Add with a negative number

--- tdd-sample-exercise/src/main/java/tdd/kata/sample/tdd_sample_exercise/StringCalculator.java
+++ tdd-sample-exercise/src/main/java/tdd/kata/sample/tdd_sample_exercise/StringCalculator.java
@@ -1,5 +1,7 @@
 package tdd.kata.sample.tdd_sample_exercise;
 
+import tdd.kata.sample.tdd_sample_exercise.exception.InvalidDataException;
+
 public class StringCalculator {
 
 	public static void main(String[] args) {
@@ -8,8 +10,9 @@ public class StringCalculator {
 
 	/**
 	 * Method to add numbers
+	 * @throws Exception 
 	 */
-	static int add(String numbers) {
+	static int add(String numbers) throws InvalidDataException {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
@@ -21,11 +24,17 @@ public class StringCalculator {
 		}
 	}
 	
-	private static int sum(String numbers, char delimitter){
+	private static int sum(String numbers, char delimitter) throws InvalidDataException{
 		int sum = 0;
+		StringBuilder negativeNumbers = new StringBuilder("");
 		for (String number : numbers.split(delimitter + "|\n")) {
+			if(Integer.parseInt(number)<0){
+				negativeNumbers.append(number).append(" ");
+			}
 			sum += Integer.parseInt(number);
 		}
+		if(negativeNumbers.length()!=0)
+			throw new InvalidDataException("negatives not allowed :"+negativeNumbers);	
 		return sum;
 	}
 
--- tdd-sample-exercise/src/test/java/tdd/kata/sample/tdd_sample_exercise/StringCalculatorTest.java
+++ tdd-sample-exercise/src/test/java/tdd/kata/sample/tdd_sample_exercise/StringCalculatorTest.java
@@ -2,64 +2,85 @@ package tdd.kata.sample.tdd_sample_exercise;
 
 import org.junit.Test;
 
+import tdd.kata.sample.tdd_sample_exercise.exception.InvalidDataException;
+
 import static org.junit.Assert.assertEquals;
 import static tdd.kata.sample.tdd_sample_exercise.StringCalculator.add;
 
 /**
  * Unit test for String Calculator
  */
-public class StringCalculatorTest
-{
-	
+public class StringCalculatorTest {
+
 	@Test
-    public void addTestEmptyString()
-    {
+	public void addTestEmptyString() throws InvalidDataException {
 		int sum = add("");
 		assertEquals(0, sum);
-    }
-	
+	}
+
 	@Test
-    public void addTestOneNumber()
-    {
+	public void addTestOneNumber() throws InvalidDataException {
 		int sum = add("1");
 		assertEquals(1, sum);
-    }
-	
+	}
+
 	@Test
-    public void addTestTwoNumbers()
-    {
+	public void addTestTwoNumbers() throws InvalidDataException {
 		int sum = add("1,2");
 		assertEquals(3, sum);
-    }
-	
+	}
+
 	@Test
-	public void addThreeNumbers(){
+	public void addTestThreeNumbers() throws InvalidDataException {
 		int sum = add("1,2,3");
 		assertEquals(6, sum);
 	}
-	
+
 	@Test
-	public void addTenNumbers(){
+	public void addTestTenNumbers() throws InvalidDataException {
 		int sum = add("1,2,3,4,5,6,7,8,9,10");
 		assertEquals(55, sum);
 	}
-	
+
 	@Test
-	public void addNumbersSeparatedByNewlineAndComma(){
+	public void addTestNumbersSeparatedByNewlineAndComma() throws InvalidDataException {
 		int sum = add("1\n2,3");
 		assertEquals(6, sum);
 	}
-	
+
 	@Test
-	public void addNumbersSeparatedByDiffDelimiters(){
+	public void addTestNumbersSeparatedByDiffDelimiters() throws InvalidDataException {
 		int sum = add("//;\n1;2");
 		assertEquals(3, sum);
 	}
-	
+
 	@Test
-	public void addNumbersSeparatedByDiffDelimitersWithNewLine(){
+	public void addTestNumbersSeparatedByDiffDelimitersWithNewLine() throws InvalidDataException {
 		int sum = add("//;\n1\n2;3");
 		assertEquals(6, sum);
 	}
+
+	@Test
+	public void addTestOneNegativeNumber() {
+		String exceptionMessage = "";
+		try {
+			add("//;\n1\n2;-3");
+		} catch (Exception e) {
+			exceptionMessage = e.getMessage();
+		}
+		assertEquals("negatives not allowed :-3 ", exceptionMessage);
+	}
+	
+	@Test
+	public void addTestMoreThanOneNegativeNumbers() {
+		String exceptionMessage = "";
+		try {
+			add("//;\n1\n2;-3;-5;-10");
+		} catch (Exception e) {
+			exceptionMessage = e.getMessage();
+		}
+		assertEquals("negatives not allowed :-3 -5 -10 ", exceptionMessage);
+	}
+
 
 }
