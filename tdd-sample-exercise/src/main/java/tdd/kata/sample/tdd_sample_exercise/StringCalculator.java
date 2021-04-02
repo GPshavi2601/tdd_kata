73390490688203bb19eb6f2a5f1890a3ee076f72
shivamkarodia <shivam.karodia@gmail.com>
2021-04-02 12:20:19 +0530

    Project setup for String Calculator Kata

---
+++ tdd-sample-exercise/src/main/java/tdd/kata/sample/tdd_sample_exercise/StringCalculator.java
@@ -0,0 +1,9 @@
+package tdd.kata.sample.tdd_sample_exercise;
+
+public class StringCalculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "String Calculator" );
+    }
+}
